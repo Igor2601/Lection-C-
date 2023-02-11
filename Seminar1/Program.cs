aa8eaae8f06efa6690d875080f2b9fbe32c60034
Igor2601 <i26p@ya.ru>
2023-02-11 14:52:11 +0300

    Квадрат числа

---
+++ Seminar1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number*number; // number *= number <=> number = number * number
+Console.WriteLine($"Квадрат числа {number} -> {sqr}({number*number})");
+int sqr1 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Квадрат числа {number} -> {sqr1}");
+
+
+
+
+
+
+
